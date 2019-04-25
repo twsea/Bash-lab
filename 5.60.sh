0d21f70f8757b26d2e5ff9402c7ae4143dca3195
Saparbekov <sultansaparbekov05@gmail.com>
2019-04-26 00:30:07 +0300

    5.60

---
+++ 5.60.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+if [ -z "$1" ]
+then
+	echo "Error, incorrect input, try again "
+else	
+	if [ "`echo \"$1\" | wc -m`" -gt "`echo \"$2\" | wc -m`" ];
+	then
+		echo "$1 longer than $2"
+	elif [ "`echo \"$2\" | wc -m`" -gt "`echo \"$1\" | wc -m`" ]
+	then
+		echo "$1 shorter than $2"
+	else
+		echo "$1 and $2 have the same length"
+	fi
+fi
